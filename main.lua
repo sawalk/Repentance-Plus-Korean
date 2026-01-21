3cf88ce7799bcf16a4b879199526f461a3f9ce69
sawalk <ham.step2@gmail.com>
2026-01-22 04:27:46 +0900

    2.25

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 REPKOR = RegisterMod("Repentance+ Korean", 1)
 local mod = REPKOR
 
-mod.version = "2.24"
+mod.version = "2.25"
 Isaac.DebugString("Starting Repentance+ Korean v" .. mod.version)    -- 디버깅
 
 mod.isRepentancePlus = REPENTANCE_PLUS or FontRenderSettings ~= nil
